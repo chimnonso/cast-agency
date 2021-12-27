12da530dd674799f98220b55a136b22d9cb92a3e
chimnonso <gabriel4amaizu@gmail.com>
2021-12-27 15:16:03 +0900

    setup db

--- app.py
+++ app.py
@@ -4,13 +4,39 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
 
+
+from models import Movie, Actor, setup_db
+from auth.auth import AuthError, requires_auth
+
 def create_app(test_config=None):
+	
+	app = Flask(__name__)
+	setup_db(app)
+	CORS(app)
 
-    app = Flask(__name__)
+	@app.after_request
+	def after_request(response):
+		response.headers.add(
+			'Access-Control-Allow-Headers',
+			'Content-Type, Authorization,true'
+		)
+		response.headers.add(
+			'Access-Control-Allow-Methods',
+            'GET, POST, PATCH, DELETE, OPTIONS'
+		)
 
-    return app
+		return response
+
+	@app.route('/test')
+	def testing():
+		return jsonify({
+            "success": True,
+            "message": "welcome"
+        })
+
+	return app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True)
+	app.run(host='0.0.0.0', port=80, debug=True)
